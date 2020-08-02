2289a1fe2203d792d03681e7bbe4c2f442b9cab8
pikou <434708082@qq.com>
2020-08-02 21:06:56 +0800

    调整

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hopscotch 的一种游戏
+
+体验地址: https://pikou1995.github.io/puzzle
--- deployment/run.sh
+++ deployment/run.sh
@@ -4,6 +4,6 @@ rm -f /etc/nginx/sites-enabled/puzzle.conf
 
 git pull
 npm install && npm run build
-cp -a puzzle.conf /etc/nginx/sites-enabled/
+cp -a deployment/puzzle.conf /etc/nginx/sites-enabled/
 
 nginx -s reload
